8b9ebcfc3693691cffd4ccc9294be2c0831ce72a
Robin Bilgil <robin@parsly.co.uk>
2014-02-22 23:41:26 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Polylinguo::Application.config.secret_key_base = '18cca253d4ffd627b0b19cd13dde650e7c22b2b53a8823ece7ae10d8b3b6c0d1892c5738e2fe06e7caa135d513cc8544abcf8506f09aafab722196486c6eef25'
